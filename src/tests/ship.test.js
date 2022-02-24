3051b627f1e40929c09c52ef5f25e8c96ac81fce
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-02-24 17:21:52 +0000

    Edit ship test

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -13,26 +13,29 @@ describe('Ship', () => {
   });
 
   test('when a ship is hit, correct value is pushed to array', () => {
-    ship.hit(1);
-    expect(ship.hits).toEqual([1]);
+    ship.hit({ posX: 3, posY: 2 });
+    expect(ship.hits).toStrictEqual([{ posX: 3, posY: 2 }]);
   });
 
-  test('when a ship is hit, correct value is pushed to array', () => {
-    ship.hit(1);
-    ship.hit(2);
-    expect(ship.hits).toEqual([1, 2]);
+  test('when a ship is hit multiple times, correct values are pushed to array', () => {
+    ship.hit({ posX: 3, posY: 2 });
+    ship.hit({ posX: 6, posY: 7 });
+    expect(ship.hits).toStrictEqual([
+      { posX: 3, posY: 2 },
+      { posX: 6, posY: 7 },
+    ]);
   });
 
   test('ship is sunk when all positions are hit', () => {
-    ship.hit(1);
-    ship.hit(2);
-    ship.hit(3);
+    ship.hit({ posX: 0, posY: 1 });
+    ship.hit({ posX: 5, posY: 3 });
+    ship.hit({ posX: 7, posY: 9 });
     expect(ship.isSunk()).toBe(true);
   });
 
   test('ship is not sunk if all positions are not hit', () => {
-    ship.hit(1);
-    ship.hit(2);
+    ship.hit({ posX: 0, posY: 1 });
+    ship.hit({ posX: 4, posY: 5 });
     expect(ship.isSunk()).toBe(false);
   });
 });
