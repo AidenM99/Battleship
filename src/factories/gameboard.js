fb1caeb9d8aa27bad0a555efeeb4f6947b501e0b
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-02-24 17:24:37 +0000

    Add gameboard class and test suite

---
+++ src/factories/gameboard.js
@@ -0,0 +1,35 @@
+import Ship from './ship';
+
+export default class Gameboard {
+  constructor() {
+    this.board = [];
+    this.ships = [];
+    this.initialiseBoard();
+  }
+
+  initialiseBoard() {
+    for (let i = 0; i < 10; i++) {
+      this.board.push([]);
+      for (let j = 0; j < 10; j++) {
+        this.board[i].push({ ship: false, shot: false });
+      }
+    }
+  }
+
+  placeShip(x, y, ship) {
+    this.board[x][y].ship = ship;
+  }
+
+  receiveAttack(x, y) {
+    this.board[x][y].shot = true;
+    if (this.board[x][y].ship instanceof Ship) {
+      this.board[x][y].ship.hit({ posX: x, posY: y });
+    }
+  }
+
+  isGameOver() {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {}
+    }
+  }
+}
---
+++ src/tests/gameboard.test.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+import Gameboard from '../factories/gameboard';
+import Ship from '../factories/ship';
+
+describe('Gameboard', () => {
+  let gameboard;
+
+  const testBoard = [];
+
+  for (let i = 0; i < 10; i++) {
+    testBoard.push([]);
+    for (let j = 0; j < 10; j++) {
+      testBoard[i].push({ ship: false, shot: false });
+    }
+  }
+
+  beforeEach(() => {
+    gameboard = new Gameboard();
+  });
+
+  test('gameboard is correctly initialised', () => {
+    expect(gameboard.board).toEqual(expect.arrayContaining(testArray));
+  });
+
+  test('ship is correctly placed', () => {
+    const carrier = new Ship(3);
+    gameboard.placeShip(0, 0, carrier);
+    expect(gameboard.board[0][0].ship).toEqual(carrier);
+  });
+
+  test('position correctly receives attack', () => {
+    gameboard.receiveAttack(0, 0);
+    expect(gameboard.board[0][0].shot).toBe(true);
+  });
+
+  test('receive attack function recognises if ship is hit on position', () => {
+    const carrier = new Ship(3);
+    gameboard.placeShip(0, 0, carrier);
+    gameboard.receiveAttack(0, 0);
+    expect(gameboard.board[0][0].ship.hits).toStrictEqual([
+      { posX: 0, posY: 0 },
+    ]);
+  });
+});
