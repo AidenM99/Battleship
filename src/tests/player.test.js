8ca1c3cd3fab610b9a4d689ccacf64de425b86fd
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-02-28 16:12:13 +0000

    Add more tests for computer random attacks and hitting the same location twice

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -3,13 +3,38 @@ import Player from '../factories/player';
 
 describe('Player', () => {
   let user;
+  let computer;
+  let randomMock;
 
   beforeEach(() => {
     user = new Player('user');
     computer = new Player('computer');
+    randomMock = jest.spyOn(global.Math, 'random');
+  });
+
+  afterEach(() => {
+    jest.spyOn(global.Math, 'random').mockRestore();
   });
 
   test('player is created', () => {
     expect(user.name).toBe('user');
+  });
+
+  test('computer can randomly attack', () => {
+    randomMock.mockReturnValue(0.2);
+    computer.randomAttack(user.gameboard);
+    expect(user.gameboard.board[2][2].shot).toBe(true);
+  });
+
+  test('computer cannot hit the same position twice', () => {
+    randomMock.mockRestore();
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        user.gameboard.board[i][j].shot = true;
+      }
+    }
+    user.gameboard.board[0][0].shot = false;
+    computer.randomAttack(user.gameboard);
+    expect(user.gameboard.board[0][0].shot).toBe(true);
   });
 });
