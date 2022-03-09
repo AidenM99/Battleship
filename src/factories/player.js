c48b8efed27f3868a60f73e233a54bd56af42e8b
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-03-09 17:33:03 +0000

    Add turn and considering attack functions to player class

--- src/factories/player.js
+++ src/factories/player.js
@@ -4,17 +4,44 @@ import Gameboard from './gameboard';
 export default class Player {
   constructor(name) {
     this.name = name;
+    this.isTurn = false;
+    this.isConsideringAttack = false;
     this.gameboard = new Gameboard();
   }
 
   attack(x, y, gameboard) {
-    if (gameboard.board[x][y].shot === true) return;
-    gameboard.receiveAttack(x, y);
+    if (gameboard.board[x][y].shot === true) return false;
+    gameboard.receiveAttack(x, y, this);
+    return true;
   }
 
   randomAttack(gameboard) {
     const x = Math.floor(Math.random() * 10);
     const y = Math.floor(Math.random() * 10);
-    gameboard.receiveAttack(x, y, gameboard, this);
+    gameboard.receiveAttack(x, y, this, gameboard);
+  }
+
+  turn() {
+    return this.isTurn;
+  }
+
+  consideringAttack() {
+    return this.isConsideringAttack;
+  }
+
+  setTurn() {
+    if (this.isTurn === false) {
+      this.isTurn = true;
+    } else {
+      this.isTurn = false;
+    }
+  }
+
+  setConsideringAttack() {
+    if (this.isConsideringAttack === false) {
+      this.isConsideringAttack = true;
+    } else {
+      this.isConsideringAttack = false;
+    }
   }
 }
