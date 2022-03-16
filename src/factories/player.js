fd1ae39d6606e893190e032a09df4685e159c175
Aiden McNaughton <aiden-99@hotmail.co.uk>
2022-03-16 21:34:19 +0000

    Make AI smarter by making logical instead of random attacks if a ship is found

--- src/factories/player.js
+++ src/factories/player.js
@@ -1,24 +1,130 @@
 /* eslint-disable class-methods-use-this */
+import { players } from '../game';
 import Gameboard from './gameboard';
 
 export default class Player {
   constructor(name) {
     this.name = name;
+    this.hits = [];
     this.isTurn = false;
     this.isConsideringAttack = false;
     this.gameboard = new Gameboard();
   }
 
   attack(x, y, gameboard) {
-    if (gameboard.board[x][y].shot === true) return false;
-    gameboard.receiveAttack(x, y, this, gameboard);
+    if (!gameboard.receiveAttack(x, y, this, gameboard)) return false;
     return true;
   }
 
+  evaluateAttack(lastShipHit, secondLastShipHit) {
+    let xShip = lastShipHit.x;
+    let yShip = lastShipHit.y;
+    let key = '-';
+
+    const operators = {
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+    };
+
+    if (secondLastShipHit === undefined) {
+      if (xShip + 1 < 10 && !players.user.gameboard.isShot(xShip + 1, yShip)) {
+        xShip++;
+      } else if (
+        xShip - 1 > -1 &&
+        !players.user.gameboard.isShot(xShip - 1, yShip)
+      ) {
+        xShip--;
+      } else if (
+        yShip + 1 < 10 &&
+        !players.user.gameboard.isShot(xShip, yShip + 1)
+      ) {
+        yShip++;
+      } else if (
+        yShip - 1 > -1 &&
+        !players.user.gameboard.isShot(xShip, yShip - 1)
+      ) {
+        yShip--;
+      }
+
+      return {
+        xShip,
+        yShip,
+      };
+    }
+
+    if (lastShipHit.x !== secondLastShipHit.x) {
+      while (players.user.gameboard.isShot(xShip, yShip)) {
+        if (xShip === 0) key = '+';
+        if (
+          players.user.gameboard.isShot(xShip, yShip) &&
+          !players.user.gameboard.isShip(xShip, yShip)
+        ) {
+          key = '+';
+        }
+
+        xShip = operators[key](xShip, 1);
+      }
+
+      return {
+        xShip,
+        yShip,
+      };
+    }
+
+    if (lastShipHit.y !== secondLastShipHit.y) {
+      while (players.user.gameboard.isShot(xShip, yShip)) {
+        if (yShip === 0) key = '+';
+        if (
+          players.user.gameboard.isShot(xShip, yShip) &&
+          !players.user.gameboard.isShip(xShip, yShip)
+        ) {
+          key = '+';
+        }
+
+        yShip = operators[key](yShip, 1);
+      }
+
+      return {
+        xShip,
+        yShip,
+      };
+    }
+
+    return false;
+  }
+
   randomAttack(gameboard) {
-    const x = Math.floor(Math.random() * 10);
-    const y = Math.floor(Math.random() * 10);
-    gameboard.receiveAttack(x, y, this, gameboard);
+    let x;
+    let y;
+
+    const activeShips = this.hits.filter(
+      (coord) =>
+        players.user.gameboard.isShip(coord.x, coord.y) &&
+        !players.user.gameboard.board[coord.x][coord.y].ship.isSunk()
+    );
+
+    if (activeShips.length > 0) {
+      const lastShipHit = activeShips[activeShips.length - 1];
+      const secondLastShipHit = activeShips[activeShips.length - 2];
+
+      const coords = this.evaluateAttack(lastShipHit, secondLastShipHit);
+
+      x = coords.xShip;
+      y = coords.yShip;
+    } else {
+      x = Math.floor(Math.random() * 10);
+      y = Math.floor(Math.random() * 10);
+    }
+
+    if (gameboard.receiveAttack(x, y, this, gameboard)) this.recordHit(x, y);
+  }
+
+  recordHit(x, y) {
+    this.hits.push({ x, y });
+  }
+
+  getHits() {
+    return this.hits;
   }
 
   getTurn() {
